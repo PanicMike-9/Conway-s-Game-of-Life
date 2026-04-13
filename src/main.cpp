1f35dfaae003e4312cfaaf50e87de13a5b6d7658
PanicMike-9 <omsaudager990@gmail.com>
2026-04-13 02:19:04 +0000

    add global const var for window height and width, create cell size and grid size logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <SFML/Graphics.hpp>
 
+constexpr const int WIN_WIDTH = 800;
+constexpr const int WIN_HEIGHT = 600;
+constexpr const int GRID_SIZE = 10;
+
 int main()
 {
     // window size and name
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Conway in SFML");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Conway's Game of Life");
 
-    // create circle
-    sf::RectangleShape square(sf::Vector2f(2.f, 2.f));
+    // cell size
+    int cell_size = WIN_WIDTH / GRID_SIZE;
+
+    // create cells
+    sf::RectangleShape cell(sf::Vector2f(cell_size, cell_size));
 
     // size between each square
     float size = 20.f;
-    square.setSize(sf::Vector2f(size - 1.f, size - 1.f));
+    cell.setSize(sf::Vector2f(size - 1.f, size - 1.f));
 
     // fill color
-    square.setFillColor(sf::Color::Yellow);
+    cell.setFillColor(sf::Color::Blue);
 
     // game loop start
     while(window.isOpen())
@@ -29,20 +36,19 @@ int main()
         window.clear();
 
         // draw the square shapes in window
-        for(int r = 0; r < 10; ++r)
+        for(int r = 0; r < GRID_SIZE; ++r)
         {
-            for(int c = 0; c < 10; ++c)
+            for(int c = 0; c < GRID_SIZE; ++c)
             {
-                // grid like look
-                square.setPosition(c * size, r * size);
+                // grid 
+                cell.setPosition(c * size, r * size);
 
                 // draw square
-                window.draw(square);
+                window.draw(cell);
             }
         }
 
         // display window
         window.display();
     }
-
 }
