f3d4e97b94bc6212ecac7e694920bdc10756b9cb
PanicMike-9 <omsaudager990@gmail.com>
2026-04-13 02:37:33 +0000

    remove previous functions and logic

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,7 @@ constexpr const int GRID_SIZE = 10;
 int main()
 {
     // window size and name
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Conway's Game of Life");
-
-    // cell size
-    int cell_size = WIN_WIDTH / GRID_SIZE;
-
-    // create cells
-    sf::RectangleShape cell(sf::Vector2f(cell_size, cell_size));
-
-    // size between each square
-    float size = 20.f;
-    cell.setSize(sf::Vector2f(size - 1.f, size - 1.f));
-
-    // fill color
-    cell.setFillColor(sf::Color::Blue);
+    sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Conway's Game of Life");
 
     // game loop start
     while(window.isOpen())
@@ -34,20 +21,7 @@ int main()
 
         // clear window
         window.clear();
-
-        // draw the square shapes in window
-        for(int r = 0; r < GRID_SIZE; ++r)
-        {
-            for(int c = 0; c < GRID_SIZE; ++c)
-            {
-                // grid 
-                cell.setPosition(c * size, r * size);
-
-                // draw square
-                window.draw(cell);
-            }
-        }
-
+        
         // display window
         window.display();
     }
